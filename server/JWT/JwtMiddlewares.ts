dced80c5631e87bd683c30e0257d6b54efd1ac8b
saiprasad <saiprasad.hegde@codecraft.co.in>
2024-08-13 09:38:19 +0530

    refactoring login and book server logic for testing purpose

--- server/JWT/JwtMiddlewares.ts
+++ server/JWT/JwtMiddlewares.ts
@@ -2,17 +2,87 @@ import express, { Request, Response, NextFunction } from "express";
 import jwt, { JwtPayload } from "jsonwebtoken";
 import "dotenv/config";
 import { config } from "dotenv";
-config();
+import { BookRepository } from "../../src/book-management/book.repository";
+import {
+  books,
+  drizzleAdapter,
+} from "../../drizzle-mysql2-orm/drizzleMysqlAdapter";
+import { IPageRequest } from "../../src/core/pagination";
+import { IBookBase } from "../../src/models/book.schema";
+import { hashPassword, comparePassword } from "./passwordHashing";
+import {
+  generateAccessToken,
+  generateRefreshToken,
+} from "./jwtTokenGenerators";
+import { IUser } from "../../src/models/member.model";
+import { IDrizzleAdapter } from "../../drizzle-mysql2-orm/drizzleMysqlAdapter";
+import { users } from "../../drizzle-mysql2-orm/schema";
+import { eq } from "drizzle-orm";
 
 declare global {
   namespace Express {
     interface Request {
-      user?: any; // Adjust the type according to your JWT payload
+      user?: any;
     }
   }
 }
 
+config();
+
+const db = drizzleAdapter.getPoolConnection();
 const app = express();
+app.use(express.json());
+
+// Registration Route
+app.post("/register", async (req, res) => {
+  try {
+    const { userName, password, role } = req.body;
+
+    const hashedPassword = await hashPassword(password);
+    const newUser = {
+      userName,
+      password: hashedPassword,
+      role,
+    };
+
+    await (await db).insert(users).values(newUser); // Insert new user into the database
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(400).end((err as Error).message);
+  }
+});
+
+// Login Route
+app.post("/login", async (req, res) => {
+  try {
+    const { userName, password } = req.body;
+
+    const user = await (await db)
+      .select()
+      .from(users)
+      .where(eq(users.userName, userName));
+    // Retrieve user by userName
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const isPasswordValid = await comparePassword(password, user[0].password);
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid credentials" });
+
+    const accessToken = generateAccessToken(user[0] as IUser);
+    const refreshToken = generateRefreshToken(user[0] as IUser);
+
+    res.json({ accessToken, refreshToken });
+  } catch (err) {
+    res.status(400).end((err as Error).message);
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+const bookRepository = new BookRepository(drizzleAdapter);
 export const authenticateJWT = (
   req: Request,
   res: Response,
@@ -48,33 +118,68 @@ export const authorizeRoles = (...roles: string[]) => {
   };
 };
 
-app.get("/books", authenticateJWT, (req, res) => {
-  // Only logged-in users can see the books
-  //select all books and send it to response
+app.get("/books", authenticateJWT, async (req, res) => {
+  try {
+    const bookId = req.query.id as string;
+    const offset = req.query.offset as string;
+    const limit = req.query.limit as string;
+    const search = req.query.search || "";
+
+    if (bookId) {
+      const book = await bookRepository.getById(Number(bookId));
+      res.status(200).json(book);
+    } else if (offset && limit) {
+      const pageRequest: IPageRequest = {
+        offset: Number(offset),
+        limit: Number(limit),
+        search: search as string,
+      };
+      const paginatedBooks = await bookRepository.list(pageRequest);
+      res.status(200).json(paginatedBooks);
+    } else {
+      const db = await drizzleAdapter.getStandaloneConnection();
+      const allBooks = await db.select().from(books);
+      res.status(200).json(allBooks);
+    }
+  } catch (error) {
+    res.status(400).json({
+      error: error instanceof Error ? error.message : "Unknown error occurred",
+    });
+  }
 });
 
 app.post(
   "/books",
   authenticateJWT,
   authorizeRoles("admin", "librarian"),
-  (req, res) => {
-    // Only admin or librarian can add books
-    const newBook = req.body;
-    //adding new book
-    res.status(201).json(newBook);
+  async (req, res) => {
+    try {
+      const bookData: IBookBase = req.body;
+      const newBook = await bookRepository.create(bookData);
+      res.status(201).json(newBook);
+    } catch (error) {
+      res.status(500).json({
+        error:
+          error instanceof Error ? error.message : "Unknown error occurred",
+      });
+    }
   }
 );
 
 app.delete(
-  "/books/:id",
+  "/books",
   authenticateJWT,
   authorizeRoles("admin", "librarian"),
-  (req, res) => {
-    // Only admin or librarian can delete books
-    const bookId = parseInt(req.params.id, 10);
-    //getBookById
-    //delete book
-
-    //else send error status
+  async (req, res) => {
+    try {
+      const bookId = Number(req.query.id as string);
+      const deletedBook = await bookRepository.delete(bookId);
+      res.status(200).json(deletedBook);
+    } catch (error) {
+      res.status(500).json({
+        error:
+          error instanceof Error ? error.message : "Unknown error occurred",
+      });
+    }
   }
 );
